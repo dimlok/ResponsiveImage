56270bad2dca4d1b9066ab3a07cdacc4e38d15b0
Weikum Kirill <kirasakuya@outlook.de>
2021-04-23 18:26:07 +0200

    - adjust first image

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -3,6 +3,15 @@ import MiddleImage from "../../img/img1.jpg";
 import SmaleImage from "../../img/img2.jpg";
 import LargeImage from "../../img/img3.jpg";
 
+const ImageBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  height: 100vh;
+`;
+
 const Cards = styled.div`
   font-size: 16px;
   margin-bottom: 70px;
@@ -42,44 +51,50 @@ interface ContentProps {}
 
 export default function Content(props: ContentProps) {
   return (
-    <Cards>
-      <CardsTitle>Cards in 3</CardsTitle>
-      <CardsRow>
-        <Card>
-          <ImageContainer>
-            <CardImage src={MiddleImage} alt="" />
-          </ImageContainer>
-          <CardContent>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a,
-            quasi earum aperiam voluptatibus commodi, blanditiis delectus
-            aspernatur necessitatibus fuga consequuntur quas accusamus atque
-            nostrum natus itaque reprehenderit beatae maiores.
-          </CardContent>
-        </Card>
-        <Card>
-          <ImageContainer>
-            <CardImage src={LargeImage} alt="" />
-          </ImageContainer>
-          <CardContent>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a,
-            quasi earum aperiam voluptatibus commodi, blanditiis delectus
-            aspernatur necessitatibus fuga consequuntur quas accusamus atque
-            nostrum natus itaque reprehenderit beatae maiores. Lorem ipsum dolor
-            sit, amet consectetur adipisicing elit. Voluptates officia vitae
-            vero sint adipisci debitis alias odit, cum, error eaque vel unde
-            voluptate accusamus quod pariatur nobis at incidunt reprehenderit.
-          </CardContent>
-        </Card>
-        <Card>
-          <ImageContainer>
-            <CardImage src={SmaleImage} alt="" />
-          </ImageContainer>
-          <CardContent>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a,
-            quasi earum aperiam voluptatibus commodi,
-          </CardContent>
-        </Card>
-      </CardsRow>
-    </Cards>
+    <>
+      <ImageBox>
+        <Image src={SmaleImage} alt="" />
+      </ImageBox>
+      <Cards>
+        <CardsTitle>Cards in 3</CardsTitle>
+        <CardsRow>
+          <Card>
+            <ImageContainer>
+              <CardImage src={MiddleImage} alt="" />
+            </ImageContainer>
+            <CardContent>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+              a, quasi earum aperiam voluptatibus commodi, blanditiis delectus
+              aspernatur necessitatibus fuga consequuntur quas accusamus atque
+              nostrum natus itaque reprehenderit beatae maiores.
+            </CardContent>
+          </Card>
+          <Card>
+            <ImageContainer>
+              <CardImage src={LargeImage} alt="" />
+            </ImageContainer>
+            <CardContent>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+              a, quasi earum aperiam voluptatibus commodi, blanditiis delectus
+              aspernatur necessitatibus fuga consequuntur quas accusamus atque
+              nostrum natus itaque reprehenderit beatae maiores. Lorem ipsum
+              dolor sit, amet consectetur adipisicing elit. Voluptates officia
+              vitae vero sint adipisci debitis alias odit, cum, error eaque vel
+              unde voluptate accusamus quod pariatur nobis at incidunt
+              reprehenderit.
+            </CardContent>
+          </Card>
+          <Card>
+            <ImageContainer>
+              <CardImage src={SmaleImage} alt="" />
+            </ImageContainer>
+            <CardContent>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+              a, quasi earum aperiam voluptatibus commodi,
+            </CardContent>
+          </Card>
+        </CardsRow>
+      </Cards>
+    </>
   );
 }
